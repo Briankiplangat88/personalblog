971c12efd0e29ea5aa7d3eef6deacd7c52cbcdcb
Brian Kiplangat <brian.kiplangat@student.moringaschool.com>
2022-03-16 10:07:35 +0300

    added the intial commit

---
+++ app/main/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import DataRequired
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Bio.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class BlogForm(FlaskForm):
+    title_blog = StringField('Title')
+    description = TextAreaField('Write a Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Write a comment', validators=[DataRequired()])
+    submit = SubmitField('Comment')
+
+
+class SubscriberForm(FlaskForm):
+    email = StringField('Your Email Address')
+    name = StringField('Enter your name',validators = [DataRequired()])
+    submit = SubmitField('Subscribe')
