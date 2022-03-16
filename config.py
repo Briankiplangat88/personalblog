00b01e7121a4aefecde0b6047fef11400afece20
Brian Kiplangat <brian.kiplangat@student.moringaschool.com>
2022-03-16 10:36:59 +0300

    add the instance

--- config.py
+++ config.py
@@ -24,15 +24,9 @@ class Config:
         pass
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
    
-    SQLALCHEMY_DATABASE_URI  ='postgresql://jwwgorxkpqcxle:434f94ccb778a0d6bf62316163b80a4daaf1a133e593d700ba47701e38d7d140@ec2-34-231-183-74.compute-1.amazonaws.com:5432/dbqdjqpuk0bapa'
-
+pass
 
 
 class TestConfig(Config):
