70266acd202635bf9b91264d3ad0b5b322bdd7bf
Brian Kiplangat <brian.kiplangat@student.moringaschool.com>
2022-03-16 10:19:23 +0300

    added tests

--- config.py
+++ config.py
@@ -30,14 +30,13 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2015@localhost/blogs'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+   
     SQLALCHEMY_DATABASE_URI  ='postgresql://jwwgorxkpqcxle:434f94ccb778a0d6bf62316163b80a4daaf1a133e593d700ba47701e38d7d140@ec2-34-231-183-74.compute-1.amazonaws.com:5432/dbqdjqpuk0bapa'
 
 
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2015@localhost/blogs_test'
+    SQLALCHEMY_DATABASE_URI = ''
     pass
 
 
@@ -49,7 +48,7 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:2015@localhost/blogs'
+    SQLALCHEMY_DATABASE_URI = ''
     DEBUG = True
 
 
